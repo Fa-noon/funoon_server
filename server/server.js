925d01cde9d4a3d77d79a1a56a87e939be53af46
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2022-09-27 18:47:30 +0500

    minor fixes

--- server/server.js
+++ server/server.js
@@ -5,18 +5,18 @@ import mongoose from 'mongoose';
 const app = express();
 
 dotenv.config({ path: __dirname + './../config.env' });
-console.log(process.env);
+// console.log(process.env);
 
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
 );
-console.log(DB);
+// console.log(DB);
 mongoose.connect(DB, {}).then(() => console.log('DB connection successful!'));
 
 app.use(json());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 app.get('/', async (req, res) => {
   res.json({ status: true, message: 'Our node.js app works' });
