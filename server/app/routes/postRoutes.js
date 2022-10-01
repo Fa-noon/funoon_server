fb831bded02ae16d5a0922333b7ca53da2b4a012
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2022-10-01 20:29:12 +0500

    Middlewares Added

--- server/app/routes/postRoutes.js
+++ server/app/routes/postRoutes.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const postController = require('../controllers/postController');
 const authController = require('../controllers/authController');
+const authMiddleware = require("middlewares/authMiddleware")
+
 
 const router = express.Router();
 
-router.post('/createPost', authController.protect,postController.uploadUserPhoto, postController.createPost);
+router.post('/createPost', authMiddleware.protect,postController.uploadUserPhoto, postController.createPost);
 
 router
   .get('/:id', postController.getPost)
-  .patch('/:id', authController.protect,authController.forbid, postController.updatePost)
-  .delete('/:id', authController.protect,authController.forbid, postController.deletePost)
-  .put("/like",authController.protect,postController.likePost);
+  .patch('/:id', authMiddleware.protect,authMiddleware.forbid, postController.updatePost)
+  .delete('/:id', authMiddleware.protect,authMiddleware.forbid, postController.deletePost)
+  .put("/like",authMiddleware.protect,postController.likePost);
 
 module.exports = router;
