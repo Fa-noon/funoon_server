4be47dfaf77315fbe5f07c6ef7c3ab5e6a4df5db
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2022-10-20 01:26:49 +0500

    Major fixes in auth Feed
    Co-Authored-By: Waseek Ahmed <58625384+WaseekSenju@users.noreply.github.com>

--- server/app/controllers/globalController.js
+++ server/app/controllers/globalController.js
@@ -1,6 +1,7 @@
 import Post from './../models/postModel.js';
 import User from './../models/userModel.js';
 import catchAsync from '../helpers/catchAsync.js';
+import AppError from './../helpers/appError.js';
 
 //----------------------- Search -----------------------------
 
@@ -47,44 +48,57 @@ export const unAuthFeed = catchAsync(async (req, res, next) => {
 
 //-------------------------------------- Auth Feed ------------------------------------------------
 export const authFeed = catchAsync(async (req, res, next) => {
-  let allPosts = [];
-  User.findById(req.user.id).exec((err, user) => {
-    if (err || !user) {
+  try {
+    let allPosts = [];
+    const user = await User.findById(req.user.id);
+    if (!user) {
       return next(new AppError('Could not load feed at this moment', 400));
     }
     if (user.interests.length == 0) {
-      Post.find()
+      const post = await Post.find()
         .populate('createdBy', 'name')
-        .sort({ dateCreated: -1 })
-        .exec((err, posts) => {
-          if (err) {
-            return next(
-              new AppError('Could not load feed at this moment', 400)
-            );
-          }
-          allPosts = posts;
-        });
+        .sort({ dateCreated: -1 });
+
+      allPosts.push(post);
     } else {
       for (let i = 0; i < user.interests.length; i++) {
-        Post.find({ tags: { $in: [user.interests[i]] } })
+        const post = await Post.find({ tags: { $in: [user.interests[i]] } })
           .populate('createdBy', 'name')
-          .sort({ dateCreated: -1 })
-          .exec((err, posts) => {
-            if (err) {
-              return next(
-                new AppError('Could not load feed at this moment', 400)
-              );
-            }
-            console.log(user.interests[i]);
-            console.log(posts);
-            allPosts = [...allPosts, posts];
-          });
+          .sort({ dateCreated: -1 });
+        if (post) {
+          //destructuring required but not usefull(also return array) because [[],[]]
+          allPosts.push(post);
+        }
       }
     }
-  });
-  res.status(200).json({
-    status: 'success',
-    allPosts,
-    Total_posts: allPosts.length,
-  });
+    //------------------------- (third) Last Step is to Add other posts which are not matched with interests------------------------
+    const unInterestedPosts = await Post.find()
+      .populate('createdBy', 'name')
+      .sort({ dateCreated: -1 });
+    if (unInterestedPosts) {
+      allPosts.push(unInterestedPosts);
+    }
+    //-------------------------Destructuring-----------------------------
+    let realPosts = [];
+    for (let i = 0; i < allPosts.length; i++) {
+      for (let j = 0; j < allPosts[i].length; j++) {
+        realPosts.push(allPosts[i][j]);
+      }
+    }
+
+    //-------------------------Remove Duplicates because 1 post can have multiple tags(interests)--------------------------
+    const key = 'id';
+    let uniquePosts = [
+      ...new Map(realPosts.map((item) => [item[key], item])).values(),
+    ];
+
+    //-----------------------------Return the response-----------------------------------------------
+    res.status(200).json({
+      status: 'success',
+      posts: uniquePosts,
+      Total_posts: uniquePosts.length,
+    });
+  } catch (error) {
+    return next(new AppError(error, 400));
+  }
 });
