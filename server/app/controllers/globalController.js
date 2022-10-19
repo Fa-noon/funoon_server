c3de9831289ac96776563aff08bf349d9b31d0a5
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2022-10-19 21:18:47 +0500

    Feed WIP

--- server/app/controllers/globalController.js
+++ server/app/controllers/globalController.js
@@ -27,3 +27,64 @@ export const Search = catchAsync(async (req, res, next) => {
     });
   });
 });
+
+//-------------------------------------- Un Auth Feed ------------------------------------------------
+export const unAuthFeed = catchAsync(async (req, res, next) => {
+  Post.find()
+    .populate('createdBy', 'name')
+    .sort({ dateCreated: -1 })
+    .exec((err, posts) => {
+      if (err) {
+        return next(new AppError('Could not load feed at this moment', 400));
+      }
+      res.status(200).json({
+        status: 'success',
+        posts,
+        Total_posts: posts.length,
+      });
+    });
+});
+
+//-------------------------------------- Auth Feed ------------------------------------------------
+export const authFeed = catchAsync(async (req, res, next) => {
+  let allPosts = [];
+  User.findById(req.user.id).exec((err, user) => {
+    if (err || !user) {
+      return next(new AppError('Could not load feed at this moment', 400));
+    }
+    if (user.interests.length == 0) {
+      Post.find()
+        .populate('createdBy', 'name')
+        .sort({ dateCreated: -1 })
+        .exec((err, posts) => {
+          if (err) {
+            return next(
+              new AppError('Could not load feed at this moment', 400)
+            );
+          }
+          allPosts = posts;
+        });
+    } else {
+      for (let i = 0; i < user.interests.length; i++) {
+        Post.find({ tags: { $in: [user.interests[i]] } })
+          .populate('createdBy', 'name')
+          .sort({ dateCreated: -1 })
+          .exec((err, posts) => {
+            if (err) {
+              return next(
+                new AppError('Could not load feed at this moment', 400)
+              );
+            }
+            console.log(user.interests[i]);
+            console.log(posts);
+            allPosts = [...allPosts, posts];
+          });
+      }
+    }
+  });
+  res.status(200).json({
+    status: 'success',
+    allPosts,
+    Total_posts: allPosts.length,
+  });
+});
--- server/app/routes/globalRoutes.js
+++ server/app/routes/globalRoutes.js
@@ -1,10 +1,15 @@
 import express from 'express';
-import {Search} from '../controllers/globalController.js';
-
+import {
+  Search,
+  unAuthFeed,
+  authFeed,
+} from '../controllers/globalController.js';
+import { protect, forbid } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 router.get('/search', Search);
-
+router.get('/unAuthFeed', unAuthFeed);
+router.get('/authFeed', protect, authFeed);
 
 export default router;
